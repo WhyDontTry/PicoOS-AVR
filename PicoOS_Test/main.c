fb3e30ef179228f8ab5e18354015e0b4154188aa
Ravil Kudashev <ravil.kudashev@mail.ru>
2024-05-01 10:36:23 +0500

    divided example into two tasks

--- PicoOS_Test/main.c
+++ PicoOS_Test/main.c
@@ -13,11 +13,11 @@
 
 
 static void initialTask(void* arg);
-static void pollTask(void *arg);
+static void ledTask(void *arg);
+static void consoleTask(void *arg);
 
-/* the initial values for the 2 tasks */
+/* the initial values for the LED task */
 static uint8_t t1 = (1 << PORTB5);
-static uint8_t t2 = 0x00;
 
 /* The start task for the initialization of the 2 other tasks.
  * This task will be destroyed, when the program runs out of the
@@ -25,28 +25,34 @@ static uint8_t t2 = 0x00;
  */
 static void initialTask(void *arg) {
 
-    posTaskCreate(pollTask, &t1, 1);
-    //posTaskCreate(pollTask, &t2, 1);
+    posTaskCreate(ledTask, &t1, 1);
+    posTaskCreate(consoleTask, NULL, 1);
 }
 
 
-static void pollTask(void *arg) {
+static void ledTask(void *arg) {
     uint8_t *valuePtr = (uint8_t*) arg;
     uint8_t value = *valuePtr;
+	DDRB = 1 << PORTB5;
 
     while(1) {
         PORTB ^= value;
-		printf("Val: %u\n", value);
 		posTaskSleep(MS(500));
     }
 }
 
-int main(void)
-{
+static void consoleTask(void *arg) {
 	usart0_init();
 	init_printf(NULL, usart0_char);
-    DDRB = 1 << PORTB5;
-    
+	
+	while(1) {
+		printf("Hi\n");
+		posTaskSleep(MS(1000));
+	}
+}
+
+int main(void)
+{   
     posInit(initialTask, NULL, 1);
     
     return 0;
